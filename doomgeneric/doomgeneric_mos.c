38d12e862bea9db5f5f84d093d42fe56d5f51380
Moldytzu <m.alex123@yahoo.com>
2023-06-02 22:48:36 +0300

    stub necessary functions

--- doomgeneric/doomgeneric_mos.c
+++ doomgeneric/doomgeneric_mos.c
@@ -1,6 +1,30 @@
 #include "doomgeneric.h"
 #include <stdio.h>
 
+void DG_Init()
+{
+}
+
+void DG_DrawFrame()
+{
+}
+
+void DG_SleepMs(uint32_t ms)
+{
+}
+
+uint32_t DG_GetTicksMs()
+{
+}
+
+int DG_GetKey(int* pressed, unsigned char* doomKey)
+{
+}
+
+void DG_SetWindowTitle(const char * title)
+{
+}
+
 int main(int argc, char **argv)
 {
     doomgeneric_Create(argc, argv);
@@ -9,6 +33,6 @@ int main(int argc, char **argv)
     {
         doomgeneric_Tick();
     }
-    
+
     return 0;
 }
